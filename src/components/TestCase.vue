8c0afc2f8450dbc78ae4f5400e97f92a5021f6f0
seveniruby <seveniruby@gmail.com>
2021-01-31 15:56:34 +0800

    add edit delete

--- src/components/TestCase.vue
+++ src/components/TestCase.vue
@@ -1,7 +1,7 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
+    :items="testcases"
     sort-by="calories"
     class="elevation-1"
   >
@@ -13,7 +13,7 @@
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on, attrs }">
             <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
+              添加
             </v-btn>
           </template>
           <v-card>
@@ -26,34 +26,31 @@
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
+                      v-model="editedItem.id"
+                      label="id"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
                       v-model="editedItem.name"
-                      label="Dessert name"
+                      label="name"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
+                      v-model="editedItem.description"
+                      label="description"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
+                      v-model="editedItem.steps"
+                      label="steps"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
+
+
                 </v-row>
               </v-container>
             </v-card-text>
@@ -102,27 +99,21 @@
       dialog: false,
       dialogDelete: false,
       headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        {text: 'id', value: 'id'},
+        {text: 'name', value: 'name' },
+        {text: 'description', value: 'description', sortable: false },
+        {text: 'steps', value: 'steps', sortable: false },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
       desserts: [],
       testcases: [],
       editedIndex: -1,
       editedItem: {
+        id: 0,
         name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        description: '',
+        steps: ''
+
       },
       defaultItem: {
         name: '',
@@ -165,101 +156,25 @@
           console.log(response)
           if(response.data.errcode===0)
             this.testcases=response.data.body
-            console.log(this.desserts)
             console.log(this.testcases)
-            this.desserts=this.testcases
         })
-
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
-        ]
-
 
       },
 
       editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
+        this.editedIndex = this.testcases.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
       },
 
       deleteItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
+        this.editedIndex = this.testcases.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialogDelete = true
       },
 
       deleteItemConfirm () {
-        this.desserts.splice(this.editedIndex, 1)
+        this.testcases.splice(this.editedIndex, 1)
         this.closeDelete()
       },
 
@@ -281,9 +196,23 @@
 
       save () {
         if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+          Object.assign(this.testcases[this.editedIndex], this.editedItem)
+          http.request({
+            method: 'put',
+            url: '/testcase',
+            data: this.editedItem
+          }).then(response=>{
+            console.log(response.data)
+          })
         } else {
-          this.desserts.push(this.editedItem)
+          this.testcases.push(this.editedItem)
+          http.request({
+            method: 'post',
+            url: '/testcase',
+            data: this.editedItem
+          }).then(response=>{
+            console.log(response.data)
+          })
         }
         this.close()
       },
